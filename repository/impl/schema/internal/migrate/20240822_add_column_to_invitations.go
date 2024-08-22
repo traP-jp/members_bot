addfeca248d45098b6263fe7ea1a752af5143bf5
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2024-08-22 22:01:37 +0900

    :bug: migrationがうまくいってなかったの修正

--- repository/impl/schema/internal/migrate/20240822_add_column_to_invitations.go
+++ repository/impl/schema/internal/migrate/20240822_add_column_to_invitations.go
@@ -21,59 +21,45 @@ type InvitationV2 struct {
 func v2(m *migrate.Migrations) {
 	m.MustRegister(
 		func(ctx context.Context, db *bun.DB) (err error) {
-			tx, err := db.BeginTx(ctx, nil)
-			if err != nil {
-				return fmt.Errorf("failed to begin tx: %w", err)
-			}
-			defer func() {
-				err = tx.Rollback()
-			}()
+			return db.RunInTx(ctx, nil, func(ctx context.Context, tx bun.Tx) error {
 
-			_, err = tx.NewRaw("ALTER TABLE invitations RENAME COLUMN id TO message_id").Exec(ctx)
-			if err != nil {
-				return fmt.Errorf("failed to rename column: %w", err)
-			}
+				_, err = tx.NewRaw("ALTER TABLE invitations RENAME COLUMN id TO message_id").Exec(ctx)
+				if err != nil {
+					return fmt.Errorf("failed to rename column: %w", err)
+				}
 
-			_, err = tx.NewRaw("ALTER TABLE invitations DROP PRIMARY KEY").Exec(ctx)
-			if err != nil {
-				return fmt.Errorf("failed to drop primary key: %w", err)
-			}
+				_, err = tx.NewRaw("ALTER TABLE invitations DROP PRIMARY KEY").Exec(ctx)
+				if err != nil {
+					return fmt.Errorf("failed to drop primary key: %w", err)
+				}
 
-			_, err = tx.NewRaw("ALTER TABLE invitations ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY").Exec(ctx)
-			if err != nil {
-				return fmt.Errorf("failed to add column: %w", err)
-			}
+				_, err = tx.NewRaw("ALTER TABLE invitations ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY").Exec(ctx)
+				if err != nil {
+					return fmt.Errorf("failed to add column: %w", err)
+				}
 
-			err = tx.Commit()
-			if err != nil {
-				return fmt.Errorf("failed to commit tx: %w", err)
-			}
-
-			return nil
+				return nil
+			})
 		},
 		func(ctx context.Context, db *bun.DB) (err error) {
-			tx, err := db.BeginTx(ctx, nil)
-			if err != nil {
-				return fmt.Errorf("failed to begin tx: %w", err)
-			}
-			defer func() { err = tx.Rollback() }()
+			return db.RunInTx(ctx, nil, func(ctx context.Context, tx bun.Tx) error {
+				_, err = tx.NewRaw("ALTER TABLE invitations DROP COLUMN id").Exec(ctx)
+				if err != nil {
+					return fmt.Errorf("failed to drop column: %w", err)
+				}
 
-			_, err = tx.NewRaw("ALTER TABLE invitations DROP COLUMN id").Exec(ctx)
-			if err != nil {
-				return fmt.Errorf("failed to drop column: %w", err)
-			}
+				_, err = tx.NewRaw("ALTER TABLE invitations RENAME COLUMN message_id TO id").Exec(ctx)
+				if err != nil {
+					return fmt.Errorf("failed to rename column: %w", err)
+				}
 
-			_, err = tx.NewRaw("ALTER TABLE invitations RENAME COLUMN message_id TO id").Exec(ctx)
-			if err != nil {
-				return fmt.Errorf("failed to rename column: %w", err)
-			}
+				_, err = tx.NewRaw("ALTER TABLE invitations ADD PRIMARY KEY (id)").Exec(ctx)
+				if err != nil {
+					return fmt.Errorf("failed to add primary key: %w", err)
+				}
 
-			_, err = tx.NewRaw("ALTER TABLE invitations ADD PRIMARY KEY (id)").Exec(ctx)
-			if err != nil {
-				return fmt.Errorf("failed to add primary key: %w", err)
-			}
-
-			return nil
+				return nil
+			})
 		},
 	)
 }
